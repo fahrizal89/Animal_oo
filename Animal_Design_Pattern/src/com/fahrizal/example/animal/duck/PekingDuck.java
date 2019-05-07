0f2ce5eb2a3ab11c6bc788bfecaa342284fbb6a6
fahrizalmulya.sentosa <fahrizalfms@gmail.com>
2019-05-07 07:57:22 +0700

    Refactor change name ModelDuck to PekingDuck

--- Animal_Design_Pattern/src/com/fahrizal/example/animal/duck/PekingDuck.java
+++ Animal_Design_Pattern/src/com/fahrizal/example/animal/duck/PekingDuck.java
@@ -3,14 +3,14 @@ package com.fahrizal.example.animal.duck;
 import com.fahrizal.example.animal.duck.fly.FlyNoWay;
 import com.fahrizal.example.animal.duck.quack.Quack;
 
-public class ModelDuck extends Duck{
-	public ModelDuck(){
+public class PekingDuck extends Duck{
+	public PekingDuck(){
 		flyBehavior = new FlyNoWay();
 		quackBehavior = new Quack();
 	}
 
 	@Override
 	public void display() {
-		System.out.println("Show Model duck");
+		System.out.println("Show Peking duck");
 	}
 }
